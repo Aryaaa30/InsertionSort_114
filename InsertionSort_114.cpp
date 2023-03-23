d19a3113a31bc82a2c59c58a3d332537b40d45d0
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-23 15:28:51 +0700

    Prosedur Input

--- InsertionSort_114.cpp
+++ InsertionSort_114.cpp
@@ -4,6 +4,26 @@ using namespace std;
 int arr[20];
 int n;
 
+void input() {
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n;
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen. \n";
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "=====================" << endl;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
