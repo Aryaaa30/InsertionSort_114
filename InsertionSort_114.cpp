86d7bed3a75ed207ff312e05f9831820f034fa3f
Aryaaa30 <arya.anugrah.hsb@gmail.com>
2023-03-23 15:34:39 +0700

    Memasukkan Bilangan

--- InsertionSort_114.cpp
+++ InsertionSort_114.cpp
@@ -22,6 +22,12 @@ void input() {
     cout << "=====================" << endl;
     cout << "Masukkan Elemen Array" << endl;
     cout << "=====================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
 }
 
 int main()
